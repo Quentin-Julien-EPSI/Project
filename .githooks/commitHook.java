60f75d4b3540792997f09f225b2a2266c4a3247b
Julien Flusin <julienflusin3@gmail.com>
2022-11-28 17:57:35 +0100

    Changement du fonctionnement du hook de pre commit

--- .githooks/commitHook.java
+++ .githooks/commitHook.java
@@ -2,17 +2,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Exo1_Hook2 {
-
+public class hook {
 	public static void main(String[] args) throws IOException {
 		String data = new String(Files.readAllBytes(Paths.get(args[0])));
-	    if(data.startsWith("ADD-")) {
-	    	System.out.println("Le message de commit commence bien par 'ADD-' !");
+	    if(data.length()>=5) {
+	    	System.out.println("Le message de commit contient au moins 5 caractères !");
 	    	System.exit(0);
 	    }else {
-	    	System.out.println("Le message de commit ne commence pas par 'ADD-' !");
+	    	System.out.println("Le message de commit comment moins de 5 caractères !");
 	    	System.exit(1);
 	    }
 	}
-
 }
