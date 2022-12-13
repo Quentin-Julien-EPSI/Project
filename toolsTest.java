e34f337f20791e08bfc5019807a179424cd2b438
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2022-12-13 21:05:50 +0100

    Creation des tests
    ajouts des test, le POM est a pars

---
+++ toolsTest.java
@@ -0,0 +1,18 @@
+package project.Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class toolsTest {
+
+	@Test
+	void getrandomTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void askQuestionTest() {
+		fail("Not yet implemented");
+	}
+
+}
