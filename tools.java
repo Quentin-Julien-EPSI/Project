86f2d29e3c03ca9a18c8b8db7c7f3790e26bb946
Julien Flusin <julienflusin3@gmail.com>
2022-12-14 01:53:58 +0100

    Création des test unitaires de la classe tools

--- tools.java
+++ tools.java
@@ -19,9 +19,13 @@ public class tools {
         return r.nextInt((max - min) + 1) + min;
     }
 	
-	public static int askQuestion() {
+	public static int askQuestion(player player) {
 		String question = "Que souhaitez-vous faire ? (1: Attaquer, 2: Utiliser un sort, 3: Utiliser un objet)";
 		int answer = Integer.parseInt(JOptionPane.showInputDialog(question));
-		return answer;
+		if(answer >=1 && answer <=3) {
+			return answer;
+		}else {
+			return 0;
+		}
 	}
 }
--- toolsTest.java
+++ toolsTest.java
@@ -5,14 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class toolsTest {
-
 	@Test
 	void getrandomTest() {
-		fail("Not yet implemented");
+		int nb1 = tools.getRandom(1, 10);
+		int nb2 = tools.getRandom(-10, -1);
+		assertTrue(nb1 >=1 && nb1 <=10);
+		assertTrue(nb2 >= -10 && nb2 <= -1);
 	}
+	
 	@Test
 	void askQuestionTest() {
-		fail("Not yet implemented");
+		player player = new player(true);
+		int answer = tools.askQuestion(player);
+		assertTrue(answer >= 1 && answer <= 3);
 	}
-
 }
