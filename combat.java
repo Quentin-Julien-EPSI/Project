65b4f25141f0953acc8bd95a41e26ffa9342fe73
Julien Flusin <julienflusin3@gmail.com>
2022-12-14 01:54:52 +0100

    correction bug combat

--- combat.java
+++ combat.java
@@ -14,18 +14,21 @@ public class combat {
 	combat(player p1, player p2){
 		ArrayList<player> players = new ArrayList<player> (Arrays.asList(p1,p2));
 		int actualPlayerIndex = 0;
-		while(p1.actualHP > 0 || p2.actualHP > 0) {
+		while(players.get(0).actualHP >= 0 && players.get(1).actualHP >= 0) {
 			player actual_player = players.get(actualPlayerIndex);
 			player other_player = players.get(get_other(actualPlayerIndex));
+			System.out.println(actual_player.name+" a actuellement "+actual_player.actualHP+ "HP");
+			System.out.println(other_player.name+" a actuellement "+other_player.actualHP+ "HP");
 			int answer = 0;
 			if(players.get(actualPlayerIndex).mob) {
 				answer = tools.getRandom(1, 2);
 			}else {
-				answer = tools.askQuestion();
+				answer = tools.askQuestion(players.get(actualPlayerIndex));
 			}
 			switch(answer){
 				case 1:
 					actual_player.attack(other_player);
+					System.out.println(actual_player.name+" vient d'infliger "+actual_player.attack+" dégats à "+other_player.name+" qui a désormais "+other_player.actualHP+" HP !");
 					actualPlayerIndex = get_other(actualPlayerIndex);
 					break;
 				case 2:
@@ -41,5 +44,12 @@ public class combat {
 					break;
 			}
 		}
+		int looserIndex = 0;
+		if(players.get(0).actualHP<=0) {
+			looserIndex = 0;
+		}else {
+			looserIndex = 1;
+		}
+		System.out.println(players.get(looserIndex).name+" a perdu contre "+players.get(get_other(looserIndex)).name);
 	}
 }
